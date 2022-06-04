4b609932cb0d834623267ff052e22dfcc3722715
Guilherme32 <38697157+Guilherme32@users.noreply.github.com>
2022-06-04 12:03:55 -0300

    Add comunicação serial

--- codigo/esp8266/serial_comm.py
+++ codigo/esp8266/serial_comm.py
@@ -7,9 +7,9 @@ from uasyncio import sleep_ms
 
 class SerialHandler:
     def __init__(self,
-                 *commands,         # : Tuple[str, Callable],
+                 *commands,         # type: Tuple[str, Callable]
                  command_prefix="!",
-                 command_max_size=9):
+                 command_max_size=8):
         self.poll = select.poll()
         self.poll.register(sys.stdin, select.POLLIN)
 
@@ -17,22 +17,37 @@ class SerialHandler:
         self.command_prefix = command_prefix
         self.command_max_size = command_max_size
 
-        self.msg = b""
+        self.msg = ""
 
-    def check_msg(self):
-        print(self.msg)
-        if len(self.msg) >= self.command_max_size:
+    def check_msg(self) -> None:
+        """ Checa a mensagem, vendo se tem um comando com o seu nome. É chamada
+        quando um caracter novo chega.
+
+        Nota: Como só é considerado a partir do momento em que o prefixo foi
+        recebido, o tamanho mínimo nessa função é 2. """
+
+        _msg = self.msg[1:]
+        if len(_msg) > self.command_max_size:
             self.msg = ""
+        else:
+            for command in self.commands:
+                if command[0] == _msg:
+                    command[1]()
 
     async def run(self):
-        while events := self.poll.poll(0):
+        """ Roda a comunicação. Espera caracteres na entrada do sistema. Nas
+        configurações padrões do micropython, o canal de comunicação padrão é
+        UART em 115200 b/s, com 8 bits de dados, nenhum de paridade e
+        1 stop bit. """
+        while True:
             await sleep_ms(50)
-            for event in events:
-                char = event[0].read(1)
+            while events := self.poll.poll(0):
+                for event in events:
+                    char = event[0].read(1)
 
-                if char == self.command_prefix:
-                    self.msg = self.command_prefix
+                    if char == self.command_prefix:
+                        self.msg = self.command_prefix
 
-                elif char[0] == self.command_prefix:
-                    self.msg += char
-                    self.check_msg()
+                    elif self.msg:      # se já tem algo na msg
+                        self.msg += char
+                        self.check_msg()
