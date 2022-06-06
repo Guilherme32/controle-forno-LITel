e6880bbb6bec2d136b34169f8fdae077b7e35ce7
Guilherme32 <38697157+Guilherme32@users.noreply.github.com>
2022-06-05 21:25:28 -0300

    add docs

--- codigo/esp8266/sensor_reader.py
+++ codigo/esp8266/sensor_reader.py
@@ -9,23 +9,32 @@ class SensorReader:
 
         self.control_pins = tuple(Pin(pin, Pin.OUT) for pin in control_pins)
         self.read_period = read_period
-        self.readings = [0 for 0 in range(2 ** len(control_pins))]
+        self.readings = [0 for _ in range(2 ** len(control_pins))]
         self.adc = ADC(adc)
 
     def get_temperature(self, index):
+        """Pega a temperatura lida pelo sensor de indice 'index'"""
+
         voltage = self.readings[index] * 2 * 1000/1024  # *2 pq usa divisor
         return 2 + voltage / 0.01       # Sensibilidade no datasheet
 
     def set_control_pins(self, value):
+        """ 
+        Seta os pinos de controle do multiplex de forma a acessar o dispositivo 
+        numero 'value'
+        """
         for c, pin in enumerate(self.control_pins):
             pin.value((value >> c) % 2)
 
     def read_temperatures(self):
+        """Le a temperatura em todos os sensores e salva no objeto"""
+
         for i in range(len(self.readings)):
             self.set_control_pins(i)
             self.readings[i] = self.adc.read()
 
     async def run(self):
+        """O loop de funcionamento do sistema de sensores"""
         while True:
             self.read_temperatures()
             await sleep_ms(self.read_period)
