f64e973b0812aa71ccedd16637e47fa00ae47715
Guilherme32 <38697157+Guilherme32@users.noreply.github.com>
2023-04-10 21:45:45 -0300

    Rewriting system in C

--- codigo/main/include/storage_nvs.h
+++
@@ -1,9 +0,0 @@
-#include <inttypes.h>
-#include "esp_system.h"
-#include "nvs_flash.h"
-#include "nvs.h"
-#include "esp_log.h"
-
-void storage_init(void);
-bool save_str(char* key, char* value);
-bool get_str(char* key, char* value, size_t len);
